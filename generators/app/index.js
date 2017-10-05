7e95057a04c0f4e6ca45eb2655187379241a7499
Jan-Sebastian Fabík <honza@fabik.org>
2017-10-05 09:39:01 +0200

    Specified basic command prompts

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,15 +6,56 @@ const yosay = require('yosay');
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(
-      yosay('Welcome to the legendary ' + chalk.red('generator-nette') + ' generator!')
-    );
+    this.log(yosay('Welcome to the legendary ' + chalk.red('Nette') + ' generator!'));
 
     const prompts = [
       {
         type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
+        name: 'modules',
+        message: 'Would you like to use modules?',
+        default: true
+      },
+      {
+        type: 'confirm',
+        name: 'basePresenter',
+        message:
+          "Would you like to use BasePresenter? (It's parent of all other Presenters).",
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'signInForm',
+        message: 'Would you like to add a sign in form?',
+        default: false
+      },
+      {
+        when: function(props) {
+          return props.signInForm;
+        },
+        type: 'list',
+        name: 'authorizator',
+        message: 'What kind of authentication would you like to use?',
+        choices: [
+          {
+            name: 'Simple authenticator (credentials are stored in config.neon)',
+            value: 'simple'
+          },
+          {
+            name: 'Nette/Database authenticator (credentials are stored in the database)',
+            value: 'netteDatabase'
+          }
+        ]
+      },
+      {
+        type: 'confirm',
+        name: 'adminer',
+        message: 'Would you like to install Adminer?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'composer',
+        message: 'Would you like to install dependencies via Composer?',
         default: true
       }
     ];
@@ -26,13 +67,29 @@ module.exports = class extends Generator {
   }
 
   writing() {
+    this.fs.copy(this.templatePath('.gitignore'), this.destinationPath('.gitignore'));
+
+    this.fs.copy(this.templatePath('.htaccess'), this.destinationPath('.htaccess'));
+
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath('composer.json'),
+      this.destinationPath('composer.json')
     );
+
+    this.fs.copy(this.templatePath('readme.md'), this.destinationPath('readme.md'));
+
+    this.fs.copy(this.templatePath('app'), this.destinationPath('app'));
+
+    this.fs.copy(this.templatePath('log'), this.destinationPath('log'));
+
+    this.fs.copy(this.templatePath('temp'), this.destinationPath('temp'));
+
+    this.fs.copy(this.templatePath('www'), this.destinationPath('www'));
   }
 
   install() {
-    this.installDependencies();
+    if (this.props.composer) {
+      this.spawnCommand('composer', ['install']);
+    }
   }
 };
