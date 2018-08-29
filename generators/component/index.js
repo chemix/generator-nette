2b966dbe2b72f9cc5235aaa15d8cbe438cf12cf4
Václav Kraus <krauva@gmail.com>
2018-08-29 11:44:26 +0200

    fix compatibility with latest yeoman

--- generators/component/index.js
+++ generators/component/index.js
@@ -2,32 +2,32 @@
 var yeoman = require('yeoman-generator');
 var _ = require('lodash');
 
-module.exports = yeoman.generators.Base.extend({
-  initializing: function () {
-    this.argument('componentName', {
-      required: true,
-      type: String,
-      desc: 'The component name'
-    });
+module.exports = class extends yeoman {
+	initializing() {
+		this.argument('componentName', {
+			required: true,
+			type: String,
+			desc: 'The component name'
+		});
 
-    this.log('You called the Nette component with the argument ' + this.componentName + '.');
-  },
+		this.log('You called the Nette component with the argument ' + this.options.componentName + '.');
+	};
 
-  writing: function () {
-    this.fs.copyTpl(
-      this.templatePath('component.php'),
-      this.destinationPath('app/components/'+ _.capitalize(this.componentName) +'/'+ this.componentName +'.php'),
-      {
-        componentName: _.capitalize(this.componentName),
-        templateFile: this.componentName,
-      }
-    );
-    this.fs.copyTpl(
-      this.templatePath('component.latte'),
-      this.destinationPath('app/components/'+ _.capitalize(this.componentName) +'/'+ this.componentName +'.latte'),
-      {
-        componentName: _.capitalize(this.componentName)
-      }
-    );
-  }
-});
+	writing() {
+		this.fs.copyTpl(
+			this.templatePath('component.php'),
+			this.destinationPath('app/components/' + _.capitalize(this.options.componentName) + '/' + this.options.componentName + '.php'),
+			{
+				componentName: _.capitalize(this.options.componentName),
+				templateFile: this.options.componentName,
+			}
+		);
+		this.fs.copyTpl(
+			this.templatePath('component.latte'),
+			this.destinationPath('app/components/' + _.capitalize(this.options.componentName) + '/' + this.options.componentName + '.latte'),
+			{
+				componentName: _.capitalize(this.options.componentName)
+			}
+		);
+	}
+};
--- generators/presenter/index.js
+++ generators/presenter/index.js
@@ -2,35 +2,33 @@
 var yeoman = require('yeoman-generator');
 var _ = require('lodash');
 
-module.exports = yeoman.generators.Base.extend({
-  initializing: function () {
-    this.argument('presenterName', {
-      required: true,
-      type: String,
-      desc: 'The presenter name'
-    });
+module.exports = class extends yeoman {
+	initializing() {
+		this.argument('presenterName', {
+			required: true,
+			type: String,
+			desc: 'The presenter name'
+		});
+		this.basePresenter = this.config.get('basePresenter');
 
-    this.basePresenter = this.config.get('basePresenter');
+		this.log('You called the Nette presenter with the argument ' + this.presenterName + '.');
+	};
 
-    this.log('You called the Nette presenter with the argument ' + this.presenterName + '.');
-  },
-
-
-  writing: function () {
-    this.fs.copyTpl(
-      this.templatePath('_Presenter.php'),
-      this.destinationPath('app/presenters/' + _.capitalize(this.presenterName) + 'Presenter.php'),
-      {
-        basePresenter: this.basePresenter,
-        presenterName: _.capitalize(this.presenterName)
-      }
-    );
-    this.fs.copyTpl(
-      this.templatePath('_default.latte'),
-      this.destinationPath('app/presenters/templates/' + _.capitalize(this.presenterName) + '/default.latte'),
-      {
-        presenterName: _.capitalize(this.presenterName)
-      }
-    );
-  }
-});
+	writing() {
+		this.fs.copyTpl(
+			this.templatePath('_Presenter.php'),
+			this.destinationPath('app/presenters/' + _.capitalize(this.presenterName) + 'Presenter.php'),
+			{
+				basePresenter: this.basePresenter,
+				presenterName: _.capitalize(this.presenterName)
+			}
+		);
+		this.fs.copyTpl(
+			this.templatePath('_default.latte'),
+			this.destinationPath('app/presenters/templates/' + _.capitalize(this.presenterName) + '/default.latte'),
+			{
+				presenterName: _.capitalize(this.presenterName)
+			}
+		);
+	}
+};
